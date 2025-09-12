e1106b16b3b03e5edb3c6c4559213f5e1ccec8c4
ROSIANE SOARES PONATH <202509031551@alunos.estacio.br>
2025-09-12 14:29:04 +0000

    commit

---
+++ SaudaçãoPersonalizada.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    char nome[50], cidade [50];
+
+    printf("Digite seu nome: ");
+    scanf("%s", nome);
+
+    printf("Digite sua cidade: ");
+    scanf("%s", cidade);
+
+    printf("Olá, %s! Você mora em %s.\n", nome, cidade);
+
+
+
+
+
+    return 0;
+}
