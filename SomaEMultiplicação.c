84dc74f14384c476fd336ac9758f94694fa12b80
ROSIANE SOARES PONATH <202509031551@alunos.estacio.br>
2025-09-12 14:37:01 +0000

    commit

---
+++ SomaEMultiplicação.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+
+    printf("Digite o primeiro número: ");
+    scanf("%d", &a);
+
+    printf("Digite o segundo número: ");
+    scanf("%d", &b);
+
+    printf("Soma = %d\n", a + b);
+    printf("Produto = %d\n", a * b);
+
+
+
+    return 0;
+}
